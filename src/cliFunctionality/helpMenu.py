9ab9029b84291bd852f9e2d4f6e33d2535c77bc4
jackbisceglia <jackbisceglia2000@gmail.com>
2021-03-31 16:17:06 -0400

    inital CLI built

---
+++ src/cliFunctionality/helpMenu.py
@@ -0,0 +1,12 @@
+def helpMenu(options) -> None:
+    # TODO: insert common use cases for CLI eventually
+    menu =''' Usage:
+    
+    General Usage:
+    mlh -command [<optional-argument>]
+    my_program [<optional-argument>]
+        
+    Popular Usage:
+    '''
+
+    print(menu)
