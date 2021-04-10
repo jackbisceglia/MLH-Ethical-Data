2210b66f5a98d0b365c5271382f339e2590dedba
Kshitijaa Jaglan <29124655+deutranium@users.noreply.github.com>
2021-04-10 05:50:54 +0530

    Added mroe facts

--- src/cliFunctionality/funFacts.py
+++ src/cliFunctionality/funFacts.py
@@ -1,5 +1,4 @@
 from colorama import *
-
 import random
 
 init()
@@ -7,6 +6,18 @@ init()
 all_ports = [
     [
         "There are 65,535 virtual ports and all of them are related to network communications.",
+        "https://helpdeskgeek.com/networking/hdg-explains-what-is-a-computer-port-what-are-they-used-for/",
+    ],
+    [
+        "TCP is used to open a connection between two devices so that data can be transferred.",
+        "https://helpdeskgeek.com/networking/hdg-explains-what-is-a-computer-port-what-are-they-used-for/",
+    ],
+    [
+        "The Internet Assigned Numbers Authority (IANA) does have a registry of ports and what they are used for.",
+        "https://helpdeskgeek.com/networking/hdg-explains-what-is-a-computer-port-what-are-they-used-for/",
+    ],
+    [
+        "A parallel port can allow for many bits to be transferred at the same time",
         "https://helpdeskgeek.com/networking/hdg-explains-what-is-a-computer-port-what-are-they-used-for/",
     ],
 ]
